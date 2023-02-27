8de4e36fec6b2458f07297ad147687d90e53d407
Sabrina Lins <83469094+sabrinaflc@users.noreply.github.com>
2023-02-27 12:01:31 -0300

    Add files via upload

---
+++ Valorant.js
@@ -0,0 +1,14 @@
+var agente = "Kay/o";
+
+if (agente == "Sage" || agente == "Killjoy" || agente == "Chamber"  ) {
+    console.log("Sentinela");
+
+} else if (agente == "Raze" || agente == "Jett" || agente == "Yoru" ||agente == "Neon" || agente == "Reyna" ||agente == "Phoenix"  ) {
+    console.log("Duelista"); 
+
+} else if (agente == "Viper" || agente == "Omen" || agente == "Cypher" || agente == "Brimstone" || agente == "Astra") {
+    console.log("Controlador"); 
+
+} else {
+    console.log("Iniciador");
+}
