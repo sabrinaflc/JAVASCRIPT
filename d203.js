cd42eab74416f03f5603c431438b1b517814c651
Sabrina Lins <83469094+sabrinaflc@users.noreply.github.com>
2023-03-02 11:36:34 -0300

    Add files via upload

---
+++ d203.js
@@ -0,0 +1,113 @@
+//1
+
+/* var meninas = ["Sabrina","Alice","Carol","Brenda"]
+var nums = [1,2,3,4,5]
+var arr3 = meninas.concat(nums);
+
+console.log(arr3) */
+
+//2
+
+/* var nums = [1,2,3,4,5]
+var nums2 = [6,7,8,9,10]
+var arr3 = nums.concat(nums2);
+
+console.log(arr3) */
+
+//3
+
+/* var armario = ["saia", "vestido", "camisa", "blusa","short","bermuda","calça","farda"]
+
+armario[4] = "meias"
+
+console.log(armario) */
+
+//4 
+
+/* var geladeira = ["Banana", "Abacaxi", "Morango","Maçã","Uva"]
+geladeira[3] = "Pêra"
+
+var organizar = geladeira.sort()
+    console.log(geladeira)
+    
+console.log("Pêra está na posição: " +  `${geladeira.indexOf("Pêra")}`)      */
+   
+
+//5
+/* 
+var musica = ["Rock", "Pop", "Eletrônica","Samba","Brega","Funk","Sertanejo"]
+musica[3] = "MPB"
+
+var organizar = musica.sort()
+    console.log(musica) 
+    
+console.log("MPB está na posição: " +  `${musica.indexOf("MPB")}`)     */
+
+//6
+
+/* var familia = ["Pai","Mãe","Irmão","Irmã","Sobrinho","Avó","Avô"]
+familia[familia.length] = "Filho"
+
+var organizar = familia.sort()
+    console.log(familia) 
+
+console.log("Filho está na posição: " +  `${familia.indexOf("Filho")}`) */
+
+//7
+/* var escola = ["Ensino Fundamental","Ensino Médio","Ensino Superior"]
+escola[escola.length] = "Doutorado"
+console.log(escola)
+var organizar = escola.sort()
+
+console.log("Doutorado está na posição: " +  `${escola.indexOf("Doutorado")}`) */
+
+//8
+
+/* var viagem = ["Natal","João Pessoa","Maceió","Recife","Olinda","Bangu", "São Paulo"]
+viagem[viagem.length] = "Eu vou"
+delete viagem[3]
+
+console.log("Eu vou está na posição para: ",`${viagem.indexOf("Eu vou")}`, viagem) */
+
+//9
+
+/* var vigesimo = [21,22,23,24,25,27]
+var new20 = vigesimo.slice(1, 5)
+console.log(new20) */
+
+//10
+
+/* var nums = [19,20,21,22,23,24]
+var newa = nums.slice(1, 3)
+console.log(newa) */
+
+//11
+
+/* var city = ["Recife", "João Pessoa", "Salvador", "Teresina"]
+var city2 = ["Maceió", "São Luís", "Fortaleza"]
+
+var arr3 = city.concat(city2);
+
+console.log(arr3) */
+
+//12
+
+/* var n1 = [20,4,3,6,10,21]
+var n2 = [1,5,7,9,33]
+
+var arr3 = n1.concat(n2);
+
+console.log(arr3) */
+
+//13
+
+/* var n1 = [1,2,3,4,5,6,7]
+
+var arr2 = n1.reverse()
+console.log(arr2) */
+
+//14
+
+/* var city = ["Recife", "João Pessoa", "Salvador", "Teresina"]
+var arr2 = city.reverse()
+console.log(arr2) */
