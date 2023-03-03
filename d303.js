b834f5331108835fea437b4ab779d1680a501df6
Sabrina Lins <83469094+sabrinaflc@users.noreply.github.com>
2023-03-03 11:05:24 -0300

    Add files via upload

---
+++ d303.js
@@ -0,0 +1,29 @@
+//1
+/* var media_anual = 8;
+var retorno = media_anual >= 7 ? "Passou de ano" : "Reprovado"
+    console.log(retorno); */
+
+//2
+/* var idade = 67;
+var retorno = idade >= 50 ? "Prioridade" : "Fila normal"
+    console.log(retorno); */
+
+//3
+/* var info = "tenho carteira";
+var retorno = info == "tenho carteira" ? "Pode dirigir" : "Está proibido"
+    console.log(retorno); */
+
+//4
+/* var nacionalidade = "brasileiro";
+var retorno = nacionalidade == "brasileiro" ? "Não é estrangeiro" : "Estrangeiro"
+    console.log(retorno); */
+
+//5
+/* var semáforo = "vermelho";
+var retorno = semáforo == "verde" || semáforo == "amarelo" ? "Pode passar" : "Não pode passar"
+    console.log(retorno); */
+
+//6
+/* var facul = 758;
+var retorno = facul >= 700 ? "Entrou na faculdade nesses cursos: \n Administração, Arquitetura, Ciências, Direito, Educação, Enfermagem, Engenharia, Farmácia, Fisioterapia, Gestão, História, Jornalismo, Logística, Medicina, Nutrição, Odontologia, Pedagogia e Psicologia." : "Tente ano que vem"
+    console.log(retorno); */
---
+++ semáforo.js
@@ -0,0 +1,3 @@
+var semáforo = "vermelho";
+var retorno = semáforo == "verde" || semáforo == "amarelo" ? "Pode passar" : "Não pode passar"
+    console.log(retorno);
